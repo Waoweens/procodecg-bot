22fd600a260a18d5cbe757443692d5f201fb8ac3
Waoweens <30313089+Waoweens@users.noreply.github.com>
2021-11-13 10:29:30 +0700

    i forgot to commit
    Co-authored-by: Faris Hafizhan Hakim <farishafiz999@gmail.com>

---
+++ index.js
@@ -0,0 +1,68 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const { token, dialogpt_token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const API_URL = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium"
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+async function query(payload) {
+	var response = await fetch(API_URL, {
+		headers: { Authorization: `Bearer ${dialogpt_token}` },
+		method: 'POST',
+		body: JSON.stringify({
+			inputs: {text: payload},
+			parameter: {
+				top_p: 0.9,
+				temperature: 1.2
+			}
+		})
+	});
+
+	const result = await response.json();
+    return result;
+}
+
+client.on('messageCreate', message => {
+	if (!message.content.startsWith('!!') || message.author.bot) return;
+
+	const args = message.content.slice('!!'.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	const text = args.join(' ')
+
+	if (command == 'help') {
+		message.channel.send({embeds: [new MessageEmbed()
+			.setColor('#fb8c00')
+			.setTitle('ProCodeCG Bot')
+			.setDescription('List of commands')
+			.addField('!!help', 'Shows this message')
+			.addField('!!chat', 'Chat with DialoGPT')
+			.setTimestamp()
+			.setFooter('ProCodeCG Bot', 'https://procodecg.com/images/procodecg-new-logo.png')
+		]});
+	}
+
+	if (command == 'chat') {
+		if (!args.length) {
+			return message.channel.send('You must provide a message to chat with DialoGPT!');
+		}
+
+		try {
+			query(text).then((response) => {
+				message.channel.send(response.generated_text);
+			});
+		}
+		catch(err) {
+			message.channel.send('An error occured while trying to tun this command.\n' + err)
+			console.log(err);
+		}
+	}
+});
+
+client.login(token);
