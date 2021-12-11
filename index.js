ca68acd137e699eed5d3894b0bccce084bfc85a9
Waoweens <30313089+Waoweens@users.noreply.github.com>
2021-12-11 10:11:10 +0700

    detect database changes and update schedule job

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const fs = require('fs');
-const {
-	Client,
-	Intents,
-	MessageEmbed,
-	Collection,
-	Permissions,
-	Guild,
-} = require('discord.js');
+const { DateTime } = require('luxon');
+const schedule = require('node-schedule');
 const { token, prefix } = require('./config.json');
 
+const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({
 	intents: [
 		Intents.FLAGS.GUILDS,
@@ -16,8 +11,6 @@ const client = new Client({
 		Intents.FLAGS.GUILD_WEBHOOKS,
 	],
 });
-
-const { initializeApp } = require('firebase/app');
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyDJFySLD_YxdEBDGt3Prb7SqvjfkNFEETw',
@@ -28,8 +21,15 @@ const firebaseConfig = {
 	appId: '1:917652846171:web:5697d2fceda71d37d7717e',
 	measurementId: 'G-TDEDHM65W4',
 };
-
+const { initializeApp } = require('firebase/app');
 const app = initializeApp(firebaseConfig);
+const {
+	getFirestore,
+	collection,
+	query,
+	onSnapshot,
+} = require('firebase/firestore');
+const db = getFirestore();
 
 client.commands = new Collection();
 const commandFiles = fs
@@ -43,6 +43,47 @@ for (const file of commandFiles) {
 client.once('ready', () => {
 	client.user.setActivity('your messages', { type: 'WATCHING' });
 	console.log('Ready!');
+
+	async function getBirthdays() {
+		const q = query(collection(db, 'birthdays'));
+		const unsubscribe = onSnapshot(q, (snapshot) => {
+			snapshot.docChanges().forEach((change) => {
+				try {
+					var bdUserId = change.doc.data().userid;
+					var bdChannelId = change.doc.data().channelid;
+					var bdGuildId = change.doc.data().guildid;
+					var bdReminder = DateTime.fromISO(change.doc.data().birthday);
+					var birthRule = new schedule.RecurrenceRule();
+
+					birthRule.tz = 'Asia/Jakarta';
+					birthRule.date = bdReminder.day;
+					birthRule.month = bdReminder.month - 1;
+					birthRule.year = bdReminder.year;
+					birthRule.hour = bdReminder.hour;
+					birthRule.minute = bdReminder.minute;
+					birthRule.second = [...Array(60).keys()];
+
+					const bdJob = schedule.scheduleJob(birthRule, () => {
+						client.channels
+							.fetch(bdChannelId)
+							.then((channel) => {
+								channel.send(
+									`<@${bdUserId}> Happy birthday! :tada:`
+								);
+							})
+							.catch((err) => {
+								console.log(err);
+							});
+						bdJob.cancel();
+					});
+
+				} catch (error) {
+					console.log(error);
+				}
+			});
+		});
+	}
+	getBirthdays();
 });
 
 client.on('messageCreate', (message) => {
@@ -58,7 +99,9 @@ client.on('messageCreate', (message) => {
 	} catch (err) {
 		console.error(err);
 		message.reply(
-			'An error occured while trying to tun this command:\n```' + err + '```'
+			'An error occured while trying to tun this command:\n```' +
+				err +
+				'```'
 		);
 	}
 });
