ad4aab098446301452a16525b6663e2fff0bc669
Waoweens <30313089+Waoweens@users.noreply.github.com>
2021-12-11 10:01:11 +0700

    make addbirthday store guildid and channelid

--- commands/addbirthday.js
+++ commands/addbirthday.js
@@ -7,25 +7,43 @@ module.exports = {
 	description: 'birthday reminder',
 	execute(message, args) {
 		var userid = message.mentions.users.first().id;
+		var channelid = message.channel.id;
+		var guildid = message.guild.id;
 
-		var birthday = DateTime.fromFormat(args.slice(1).join(' '), 'dd/MM/yyyy HH:mm')
-			.setZone('Asia/Jakarta')
+		var birthday = DateTime.fromFormat(
+			args.slice(1).join(' '),
+			'dd/MM/yyyy HH:mm'
+		).setZone('Asia/Jakarta');
 
 		if (!args.length) {
-			return message.channel.send('Please mention a user and provide a birthday with format `@mention dd/MM/yyyy HH:mm`.');
+			return message.channel.send(
+				'Please mention a user and provide a birthday with format `@mention dd/MM/yyyy HH:mm`.'
+			);
 		} else if (!birthday.isValid) {
-			return message.channel.send('Invalid arguments! Please mention a user and provide a birthday with format `@mention dd/MM/yyyy HH:mm`.');
-		} else {addBirthday()}
+			return message.channel.send(
+				'Invalid arguments! Please mention a user and provide a birthday with format `@mention dd/MM/yyyy HH:mm`.'
+			);
+		} else {
+			addBirthday();
+		}
 
 		async function addBirthday() {
 			try {
 				var docRef = await addDoc(collection(db, 'birthdays'), {
 					userid: userid,
+					channelid: channelid,
+					guildid: guildid,
 					birthday: birthday.toString(),
 				});
-				message.channel.send('Successfully added your birthday! ' + docRef.id);
+				message.channel.send(
+					'Successfully added your birthday! ' + docRef.id
+				);
 			} catch (err) {
-				message.channel.send('An error has occured while trying to add your birthday:\n```'+err+'```');
+				message.channel.send(
+					'An error has occured while trying to add your birthday:\n```' +
+						err +
+						'```'
+				);
 			}
 		}
 	},
